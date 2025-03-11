c6c0129cec9c98df1d73d89343b88724b1e4c49a
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-03-11 13:00:59 +0530

    refactor ♻️: Improve code safety
    - Removed unnecessary type casting in props to improve code safety and quality.

--- components/landing/book-appointment/InstantAppointment.tsx
+++ components/landing/book-appointment/InstantAppointment.tsx
@@ -14,7 +14,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 	props,
 	ref
 ) {
-	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props as any} />;
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 const BookAppointment: React.FC = () => {
